19b2c7e5b4bb568d96bc97eef1ea5ba21fb7c642
Eva Szatmari <szateva@gmail.com>
2024-10-22 23:46:18 +0100

    started on HighLow, some ideas written as a TODO.

--- src/com/eva/maths/problems/SquareNos.java
+++ src/com/eva/maths/problems/SquareNos.java
@@ -1,4 +1,4 @@
-package com.eva.wloop;/*
+package com.eva.maths.problems;/*
 Write a program that outputs the squares of all the numbers from 1 to 10, i.e., the output will be the
 numbers 1, 4, 9, 16 and so on up to 100.
  */
---
+++ src/com/eva/wloop/HighLow.java
@@ -0,0 +1,16 @@
+package com.eva.wloop;
+
+/*
+Modify your program from the previous exercise so that it outputs \Yes" when the numbers are consecutive,
+regardless of whether they go up or down. For example, both \2,3,4,5,6,-1" and \10,9,8,7,-
+1" should now result in \Yes".
+ */
+
+/* TODO
+- consecutive = "No"
+if for loop all Yes or all No consecutive = "Yes"
+return consecutive
+ */
+
+public class HighLow {
+}
