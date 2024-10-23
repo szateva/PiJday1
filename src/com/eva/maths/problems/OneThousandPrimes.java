6d3718becca174bf7a9bc5f896fa130f9c7441db
Eva Szatmari <szateva@gmail.com>
2024-10-23 23:09:47 +0100

    modified the end of while as the printing went on to the 1001st prime (zero indexing!) Also, num initialized to 0 for consistency.

--- src/com/eva/maths/problems/OneThousandPrimes.java
+++ src/com/eva/maths/problems/OneThousandPrimes.java
@@ -7,8 +7,8 @@ Writes on screen the first 1,000 primes.
 public class OneThousandPrimes {
     public static void main(String[] args) {
         int countPrimes = 0;
-        int num = 1;
-        while (countPrimes <= 1000) {
+        int num = 0;
+        while (countPrimes < 1000) {
             num++;
             if (Prime2.isPrime(num)){
                 System.out.println(num + ", ");
