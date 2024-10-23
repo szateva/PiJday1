286c7abf7894d417bb74581b4aaafbd5a19a3472
Eva Szatmari <szateva@gmail.com>
2024-10-23 23:02:47 +0100

    Printing to screen the first 1000 primes using Prime2's isPrime method

--- src/com/eva/maths/problems/OneThousandPrimes.java
+++ src/com/eva/maths/problems/OneThousandPrimes.java
@@ -5,4 +5,15 @@ Writes on screen the first 1,000 primes.
  */
 
 public class OneThousandPrimes {
+    public static void main(String[] args) {
+        int countPrimes = 0;
+        int num = 1;
+        while (countPrimes <= 1000) {
+            num++;
+            if (Prime2.isPrime(num)){
+                System.out.println(num + ", ");
+                countPrimes++;
+            }
+        }
+    }
 }
