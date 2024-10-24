b57993f75231a9414473a8821ffc27050f7c291b
Eva Szatmari <szateva@gmail.com>
2024-10-24 23:38:35 +0100

    Jotted down ideas in TODOs for implementing OptimusPrime and Pi.

---
+++ src/com/eva/maths/problems/OptimusPrime.java
@@ -0,0 +1,17 @@
+package com.eva.maths.problems;
+
+/*
+Write a program that reads an integer number from the user, and then outputs the closest prime
+number. If there are two prime numbers at the same distance, it should output both. For instance,
+if the user enters 5116, the output should be 5113 and 5119.
+ */
+
+/*TODO
+- read in integer, using scanner
+- loop backwards from user input number until found a prime (method?)
+- loop forwards from user input number until found a prime (method?)
+- keep a count in each case and print the closer, if equal, print both
+ */
+
+public class OptimusPrime {
+}
---
+++ src/com/eva/maths/problems/Pi.java
@@ -0,0 +1,25 @@
+package com.eva.maths.problems;
+
+/*
+Pi, the ratio of a circle's circumference to its diameter, can be computed by adding the following
+terms:
+
+Create a program that asks the user for a number n and then calculates the partial addition of n terms
+of this infinite sum. How many terms do you need to get the first three digits right (3.14)? How
+many for the first 10 digits (3.14159265358. . . )?
+Hint: to represent fractions in the computer, you can use floating-point numbers. A suitable data type
+for floating point numbers in Java is called double (for \double-precision floating-point numbers").
+You can use the standard arithmetic operators \+", \-", \*", \/", \>", . . . to work with values of
+type double. Doing maths with floating-point numbers will involve small rounding errors, but can be
+done very efficiently in a modern computer.
+ */
+
+/*TODO
+- take input from the user using Scanner
+- do a for loop for the sum (up to user input times)
+- += (4*(-1)^i)/(2i+1)
+- use Math.pow(-1, i)
+
+ */
+public class Pi {
+}
